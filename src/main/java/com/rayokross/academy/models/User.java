34e11c87103147108165081d0dbe0e7cf70c708e
angelmnndez <amenendezleyenda@gmail.com>
2026-03-07 16:01:59 +0100

    User Entity

---
+++ src/main/java/com/rayokross/academy/models/User.java
@@ -0,0 +1,114 @@
+package com.rayokross.academy.models;
+
+import java.sql.Blob;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+
+@Entity(name = "USERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Lob
+    private Blob profilePhoto;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String password, String... roles) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.roles = List.of(roles);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Blob getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(Blob profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    public boolean getHasProfilePicture() {
+        if (this.profilePhoto != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
