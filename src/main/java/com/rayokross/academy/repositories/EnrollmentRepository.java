67e4f1dc58693e1558555ea32478d4e257c22cbb
Omar <malcomjrmartin@gmail.com>
2026-03-07 16:59:57 +0100

    EnrollmentRepository.java added

---
+++ src/main/java/com/rayokross/academy/repositories/EnrollmentRepository.java
@@ -0,0 +1,14 @@
+package com.rayokross.academy.repositories;
+
+import com.rayokross.academy.models.Course;
+import com.rayokross.academy.models.Enrollment;
+import com.rayokross.academy.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+
+    List<Enrollment> findByUser(User user);
+}
