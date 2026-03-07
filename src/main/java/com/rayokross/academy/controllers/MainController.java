4be995b461740fbc85e3d4975cc216340b3de4f3
angelmnndez <amenendezleyenda@gmail.com>
2026-03-07 15:20:38 +0100

    MainController Modification

--- src/main/java/com/rayokross/academy/controllers/MainController.java
+++ src/main/java/com/rayokross/academy/controllers/MainController.java
@@ -1,10 +1,21 @@
 package com.rayokross.academy.controllers;
 
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.rayokross.academy.models.Course;
 import com.rayokross.academy.services.CourseService;
 
 @Controller
@@ -32,6 +43,28 @@ public class MainController {
         model.addAttribute("course", courseService.findAll());
         return "courses";
 
+    }
+
+    @GetMapping("/course/{id}/image")
+    public ResponseEntity<Object> downloadImage(@PathVariable long id) throws SQLException {
+        Optional<Course> op = courseService.findById(id);
+
+        if (op.isPresent() && op.get().getImage() != null) {
+            Blob image = op.get().getImage();
+            Resource imageFile = new InputStreamResource(image.getBinaryStream());
+
+            // Detección del tipo de archivo según los apuntes de la URJC
+            MediaType mediaType = MediaTypeFactory
+                    .getMediaType(imageFile)
+                    .orElse(MediaType.IMAGE_JPEG);
+
+            return ResponseEntity
+                    .ok()
+                    .contentType(mediaType)
+                    .body(imageFile);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
