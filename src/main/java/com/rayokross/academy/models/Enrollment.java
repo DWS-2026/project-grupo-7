c86e075bd523827162ca7a06064d8dae0bd51505
Omar <malcomjrmartin@gmail.com>
2026-03-07 16:57:58 +0100

    Enrollment.java added

---
+++ src/main/java/com/rayokross/academy/models/Enrollment.java
@@ -0,0 +1,72 @@
+package com.rayokross.academy.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Enrollment {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private User user;
+
+    private Course course;
+
+    private LocalDate enrollmentDate;
+
+    private boolean completed;
+
+    public Enrollment() {
+
+    }
+
+    public Enrollment(User user, Course course) {
+        this.user = user;
+        this.course = course;
+        this.enrollmentDate = LocalDate.now();
+        this.completed = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public LocalDate getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(LocalDate enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
