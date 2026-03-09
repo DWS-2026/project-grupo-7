2f2b6a3e968d11d5b1109a5777f077e7df01abaf
angelmnndez <amenendezleyenda@gmail.com>
2026-03-09 11:23:55 +0100

    Course & User

--- src/main/java/com/rayokross/academy/models/User.java
+++ src/main/java/com/rayokross/academy/models/User.java
@@ -3,14 +3,7 @@ package com.rayokross.academy.models;
 import java.sql.Blob;
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
+import jakarta.persistence.*;
 
 @Entity(name = "USERS")
 public class User {
