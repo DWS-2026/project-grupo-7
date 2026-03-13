69c8a77530096a911582e8c1955a8c636733b70a
Omar <malcomjrmartin@gmail.com>
2026-03-13 12:15:22 +0100

    Course.java updated

--- src/main/java/com/rayokross/academy/models/Course.java
+++ src/main/java/com/rayokross/academy/models/Course.java
@@ -9,16 +9,7 @@ import java.util.Locale;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
-import jakarta.persistence.OneToMany;
-
+import jakarta.persistence.*;
 @Entity
 public class Course {
 
