bb16187eb1d78b02342e689bc3c15c4b4d74b657
Omar <malcomjrmartin@gmail.com>
2026-03-20 10:17:02 +0100

    Logger added CartService.java

--- src/main/java/com/rayokross/academy/services/CartService.java
+++ src/main/java/com/rayokross/academy/services/CartService.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.rayokross.academy.models.Course;
 
 @Service
 @SessionScope
 public class CartService {
+
+    private static final Logger log = LoggerFactory.getLogger(CartService.class);
 
     private List<Course> cartCourses = new ArrayList<>();
 
@@ -20,28 +24,29 @@ public class CartService {
 
     public void addCourse(Course course) {
 
-        boolean alreadyExists = cartCourses.stream()
-                .anyMatch(c -> c.getId().equals(course.getId()));
-                
+        boolean alreadyExists = cartCourses.stream().anyMatch(c -> c.getId().equals(course.getId()));
         if (!alreadyExists) {
             cartCourses.add(course);
+            log.info("Course ID {} added to the cart.", course.getId());
         }
     }
 
     public void removeCourse(Long courseId) {
-        cartCourses.removeIf(course -> course.getId().equals(courseId));
+        boolean removed = cartCourses.removeIf(course -> course.getId().equals(courseId));
+        if (removed) {
+            log.info("Course ID {} removed from cart.", courseId);
+        }
     }
 
     public double getTotalPrice() {
-        return cartCourses.stream()
-                .mapToDouble(Course::getPrice) 
-                .sum();
+        return cartCourses.stream().mapToDouble(Course::getPrice).sum();
     }
 
     public void clearCart() {
         cartCourses.clear();
+        log.info("Session cart cleared.");
     }
-    
+
     public int getSize() {
         return cartCourses.size();
     }
