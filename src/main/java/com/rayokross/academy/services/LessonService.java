50a84caf48e7d84b30d9cc67996c2f07de89f4c1
Omar <malcomjrmartin@gmail.com>
2026-03-20 10:31:17 +0100

    Logger added LessonService.java

--- src/main/java/com/rayokross/academy/services/LessonService.java
+++ src/main/java/com/rayokross/academy/services/LessonService.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.rayokross.academy.models.Lesson;
 import com.rayokross.academy.repositories.LessonRepository;
@@ -11,15 +13,19 @@ import com.rayokross.academy.repositories.LessonRepository;
 @Service
 public class LessonService {
 
+    private static final Logger log = LoggerFactory.getLogger(LessonService.class);
+
     @Autowired
     private LessonRepository lessonRepository;
 
     public void save(Lesson lesson) {
         lessonRepository.save(lesson);
+        log.info("Lesson saved. ID: {}", lesson.getId());
     }
 
     public void deleteById(Long id) {
         lessonRepository.deleteById(id);
+        log.info("Lesson with ID {} deleted.", id);
     }
 
     public Optional<Lesson> findById(Long id) {
