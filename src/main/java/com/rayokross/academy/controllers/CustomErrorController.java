76ccdf262b4fea8bb40e4fd13a9c4a7e67150859
danicroko <dani.fernantom@gmail.com>
2026-03-27 16:51:16 +0100

    CustomErrorController translated to English

--- src/main/java/com/rayokross/academy/controllers/CustomErrorController.java
+++ src/main/java/com/rayokross/academy/controllers/CustomErrorController.java
@@ -20,7 +20,7 @@ public class CustomErrorController implements ErrorController {
     public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
-        model.addAttribute("pageTitle", "Ups, algo salió mal");
+        model.addAttribute("pageTitle", "Oops, something went wrong");
 
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
@@ -28,28 +28,28 @@ public class CustomErrorController implements ErrorController {
             Object requestUri = request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
 
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                log.warn("Error 404: Página no encontrada - URI: {}", requestUri);
-                model.addAttribute("errorMessage", "Lo sentimos, la página que buscas no existe o ha sido movida.");
+                log.warn("Error 404: Page not found - URI: {}", requestUri);
+                model.addAttribute("errorMessage", "Sorry, the page you are looking for does not exist or has been moved.");
                 return "error/404";
+                
             } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                log.error("Error 500: Error interno del servidor - URI: {}", requestUri);
-                model.addAttribute("errorMessage",
-                        "Ha ocurrido un error interno en el servidor. Estamos trabajando en ello.");
+                log.error("Error 500: Internal server error - URI: {}", requestUri);
+                model.addAttribute("errorMessage", "An internal server error has occurred. We are working on it.");
                 return "error/500";
+                
             } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
-                log.warn("Error 403: Acceso denegado - URI: {}", requestUri);
-                model.addAttribute("errorMessage", "No tienes permisos suficientes para acceder a esta sección.");
+                log.warn("Error 403: Access denied - URI: {}", requestUri);
+                model.addAttribute("errorMessage", "You do not have sufficient permissions to access this section.");
                 return "error/403";
 
             } else if (statusCode == HttpStatus.BAD_REQUEST.value()) {
-                log.warn("Error 400: Petición incorrecta o URL mal formada - URI: {}", requestUri);
-                model.addAttribute("errorMessage",
-                        "La dirección que has introducido no es válida o tiene un formato incorrecto.");
+                log.warn("Error 400: Bad request or malformed URL - URI: {}", requestUri);
+                model.addAttribute("errorMessage", "The address you entered is invalid or has an incorrect format.");
                 return "error/400";
             }
         }
 
-        model.addAttribute("errorMessage", "Ha ocurrido un error inesperado.");
+        model.addAttribute("errorMessage", "An unexpected error has occurred.");
         return "error/generic";
     }
 }
