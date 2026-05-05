b7c434d82b0ea2f0635874a50630825477eb444d
Omar <malcomjrmartin@gmail.com>
2026-05-05 10:33:32 +0200

    UserDTO

---
+++ src/main/java/com/rayokross/academy/dtos/UserDTO.java
@@ -0,0 +1,11 @@
+package com.rayokross.academy.dtos;
+
+import java.util.List;
+
+public record UserDTO(
+        Long id,
+        String firstName,
+        String lastName,
+        String email,
+        List<String> roles) {
+}
